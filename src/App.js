c32f54b02c89ac6770d2024570075c56b2e6062c
CharuGopi98 <charuforprofession98@gmail.com>
2023-04-08 15:45:23 +0530

    deleted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./Components/Home";
 
 const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route exact path="/" Component={Home} />
-      </Routes>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
